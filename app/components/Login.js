da21ade3bedc181f77f06fb592f54f274bc364dc
Billy Vong <billy@opstarts.com>
2016-07-24 14:04:46 -0700

    Login form

--- app/components/Login.js
+++ app/components/Login.js
@@ -1,17 +1,66 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import styles from './Home.css';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import autobind from 'autobind-decorator';
+import {
+  observer,
+  inject,
+} from 'mobx-react';
+import styles from './Home.css';
 
+@inject('authStore')
+@autobind
+@observer
 export default class Login extends Component {
+  login(e) {
+    const {
+      authStore,
+    } = this.props;
+    e.preventDefault();
+    authStore.login();
+  }
+
+  handleSelect(e, index, value) {
+    const {
+      authStore,
+    } = this.props;
+
+    authStore.setProvider(value);
+  }
+
+  handleUsername(e, value) {
+    const {
+      authStore,
+    } = this.props;
+
+    authStore.username = e.target.value;
+  }
+
+  handlePassword(e, value) {
+    const {
+      authStore,
+    } = this.props;
+
+    authStore.password = e.target.value;
+  }
+
   render() {
+    const {
+      authStore,
+    } = this.props;
+
     return (
-      <div className={styles.container}>
+      <form
+        className={styles.container}
+        onSubmit={this.login}
+      >
         <div>
           <SelectField
+            value={authStore.provider}
+            onChange={this.handleSelect}
             autowidth
           >
             <MenuItem
@@ -27,24 +76,29 @@ export default class Login extends Component {
 
         <TextField
           hintText="Username"
+          onChange={this.handleUsername}
+          value={authStore.username}
+          name="username"
           floatingLabelText="Username"
-          floatingLabelFixed
         />
         <br />
 
         <TextField
           hintText="Password"
+          onChange={this.handlePassword}
+          value={authStore.password}
           floatingLabelText="Password"
           type="password"
+          name="password"
         />
         <br />
 
         <RaisedButton
+          type="submit"
           label="Login"
           primary
         />
-
-      </div>
+      </form>
     );
   }
 }
