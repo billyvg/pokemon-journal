01c0d2a117309785e9f85f1d0c098b6c84383cbe
Billy Vong <billy@opstarts.com>
2016-07-24 14:05:17 -0700

    Show Login if not authed

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import Home from '../components/Home';
+import Journal from '../components/Journal';
+import {
+  inject,
+  observer,
+} from 'mobx-react';
 
+@inject('authStore')
+@observer
 export default class HomePage extends Component {
   render() {
-    return (
-      <Home />
-    );
+    const {
+      authStore,
+    } = this.props;
+
+    if (authStore.authed) {
+      return <Journal />;
+    }
+
+    return <Home />;
   }
 }
