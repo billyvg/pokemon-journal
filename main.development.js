efdbbb43a06fa3b9280a3f0e13af5302f76d21c7
Billy Vong <billy@opstarts.com>
2016-07-26 19:38:29 -0700

    Allow "Inspect Element" in non-dev

--- main.development.js
+++ main.development.js
@@ -53,17 +53,18 @@ app.on('ready', async () => {
 
   if (process.env.NODE_ENV === 'development') {
     mainWindow.openDevTools();
-    mainWindow.webContents.on('context-menu', (e, props) => {
-      const { x, y } = props;
-
-      Menu.buildFromTemplate([{
-        label: 'Inspect element',
-        click() {
-          mainWindow.inspectElement(x, y);
-        }
-      }]).popup(mainWindow);
-    });
   }
+
+  mainWindow.webContents.on('context-menu', (e, props) => {
+    const { x, y } = props;
+
+    Menu.buildFromTemplate([{
+      label: 'Inspect element',
+      click() {
+        mainWindow.inspectElement(x, y);
+      }
+    }]).popup(mainWindow);
+  });
 
   if (process.platform === 'darwin') {
     template = [{
